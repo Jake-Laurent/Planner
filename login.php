29fe8cdad3794a96dc6df59de6f2bda6a805483a
Neal <44650541+rezenddd@users.noreply.github.com>
2021-05-01 22:41:36 -0500

    Add files via upload

---
+++ login.php
@@ -0,0 +1,104 @@
+<?php
+
+session_start();
+ 
+if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+    header("header: Homepage.html");
+    exit;
+}
+ 
+require_once "config.php";
+ 
+$name = $password = "";
+$name_err = $password_err = $login_err = "";
+ 
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+ 
+    if(empty(trim($_POST["name"]))){
+        $name_err = "Please enter username.";
+    } else{
+        $name = trim($_POST["name"]);
+    }
+    
+    if(empty(trim($_POST["password"]))){
+        $password_err = "Please enter your password.";
+    } else{
+        $password = trim($_POST["password"]);
+    }
+
+    if(empty($name_err) && empty($password_err)){
+
+        $sql = "SELECT User_id, name, password FROM users WHERE name = ?";
+        
+        if($stmt = mysqli_prepare($link, $sql)){
+ 
+            mysqli_stmt_bind_param($stmt, "s", $param_name);
+
+            $param_name = $name;
+
+            if(mysqli_stmt_execute($stmt)){
+
+                mysqli_stmt_store_result($stmt);
+
+                if(mysqli_stmt_num_rows($stmt) == 1){                    
+
+                    mysqli_stmt_bind_result($stmt, $id, $name, $hashed_password);
+                    if(mysqli_stmt_fetch($stmt)){
+                        if(password_verify($password, $hashed_password)){
+
+                            session_start();
+ 
+                            $_SESSION["loggedin"] = true;
+                            $_SESSION["User_id"] = $User_id;
+                            $_SESSION["name"] = $name;                            
+
+                            header("Student Planner");
+                        } else{
+
+                            $login_err = "Invalid username or password.";
+                        }
+                    }
+                } else{
+
+                    $login_err = "Invalid username or password.";
+                }
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+
+            mysqli_stmt_close($stmt);
+        }
+    }
+
+    mysqli_close($link);
+}
+?>
+ 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+</head>
+<body>
+        <h2>Login</h2>
+
+        <form>
+            <div class="form-group">
+                <label>Username</label>
+                <input type="text" name="name" class="form-control <?php echo (!empty($name_err)) ? 'is-invalid' : ''; ?>" value="">
+                <span class="invalid-feedback"><?php echo $name_err; ?></span>
+            </div>    
+            <div class="form-group">
+                <label>Password</label>
+                <input type="password" name="password" class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>">
+                <span class="invalid-feedback"><?php echo $password_err; ?></span>
+            </div>
+            <div class="form-group">
+                <input type="submit" class="btn btn-primary" value="Login">
+            </div>
+            <p>Don't have an account? <a href="register.php">Sign up now</a>.</p>
+        </form>
+    </div>
+</body>
+</html>
---
+++ register.php
@@ -0,0 +1,167 @@
+<?php
+
+require_once "config.php";
+
+$name = $password = $confirm_password = $email = "";
+$name_err = $password_err = $confirm_password_err = $email_err = "";
+ 
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+ 
+   
+    if(empty(trim($_POST["name"]))){
+        $name_err = "Please enter a username.";
+    } else{
+        
+        $sql = "SELECT User_id FROM users WHERE name = ?";
+        
+        if($stmt = mysqli_prepare($link, $sql)){
+            
+            mysqli_stmt_bind_param($stmt, "s", $param_name);
+            
+            
+            $param_name = trim($_POST["name"]);
+            
+            
+            if(mysqli_stmt_execute($stmt)){
+                
+                mysqli_stmt_store_result($stmt);
+                
+                if(mysqli_stmt_num_rows($stmt) == 1){
+                    $name_err = "This username is already taken.";
+                } else{
+                    $name = trim($_POST["name"]);
+                }
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+
+            
+            mysqli_stmt_close($stmt);
+        }
+    }
+	
+	
+	if(empty(trim($_POST["email"]))){
+        $email_err = "Please enter a email.";
+    } else{
+        
+        $sql = "SELECT User_id FROM users WHERE email = ?";
+        
+        if($stmt = mysqli_prepare($link, $sql)){
+            
+            mysqli_stmt_bind_param($stmt, "s", $param_username);
+            
+            
+            $param_email = trim($_POST["email"]);
+            
+            
+            if(mysqli_stmt_execute($stmt)){
+                
+                mysqli_stmt_store_result($stmt);
+                
+                if(mysqli_stmt_num_rows($stmt) == 1){
+                    $email_err = "This email is already in use.";
+                } else{
+                    $email = trim($_POST["email"]);
+                }
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+
+            
+            mysqli_stmt_close($stmt);
+        }
+    }
+    
+    
+    
+    if(empty(trim($_POST["password"]))){
+        $password_err = "Please enter a password.";     
+    } elseif(strlen(trim($_POST["password"])) < 6){
+        $password_err = "Password must have atleast 6 characters.";
+    } else{
+        $password = trim($_POST["password"]);
+    }
+    
+    
+    if(empty(trim($_POST["confirm_password"]))){
+        $confirm_password_err = "Please confirm password.";     
+    } else{
+        $confirm_password = trim($_POST["confirm_password"]);
+        if(empty($password_err) && ($password != $confirm_password)){
+            $confirm_password_err = "Password did not match.";
+        }
+    }
+    
+    
+    if(empty($username_err) && empty($password_err) && empty($confirm_password_err) && empty($email_err)){
+        
+        
+        $sql = "INSERT INTO users (user_id, name, password, email) VALUES (?, ?)";
+         
+        if($stmt = mysqli_prepare($link, $sql)){
+            
+            mysqli_stmt_bind_param($stmt, "ss", $param_name, $param_password);
+            
+            
+            $param_name = $name;
+            $param_password = password_hash($password, PASSWORD_DEFAULT); 
+            
+            
+            if(mysqli_stmt_execute($stmt)){
+                
+                header("location: login.php");
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+
+            
+            mysqli_stmt_close($stmt);
+        }
+    }
+    
+    
+    mysqli_close($link);
+}
+?>
+ 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Sign Up</title>
+</head>
+<body>
+    <div class="wrapper">
+        <h2>Sign Up</h2>
+        <p>Please fill this form to create an account.</p>
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>
+            <div class="form-group">
+                <label>Username</label>
+                <input type="text" name="name" class="form-control <?php echo (!empty($name_err)) ? 'is-invalid' : ''; ?>" value="">
+                <span class="invalid-feedback"><?php echo $name_err; ?></span>
+            </div> 
+			<div class="form-group">
+                <label>Email</label>
+                <input type="text" name="email" class="form-control <?php echo (!empty($email_err)) ? 'is-invalid' : ''; ?>" value="">
+                <span class="invalid-feedback"><?php echo $email_err; ?></span>
+            </div> 
+            <div class="form-group">
+                <label>Password</label>
+                <input type="password" name="password" class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>" value="">
+                <span class="invalid-feedback"><?php echo $password_err; ?></span>
+            </div>
+            <div class="form-group">
+                <label>Confirm Password</label>
+                <input type="password" name="confirm_password" class="form-control <?php echo (!empty($confirm_password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $confirm_password; ?>">
+                <span class="invalid-feedback"><?php echo $confirm_password_err; ?></span>
+            </div>
+            <div class="form-group">
+                <input type="submit" class="btn btn-primary" value="Submit">
+                <input type="reset" class="btn btn-secondary ml-2" value="Reset">
+            </div>
+            <p>Already have an account? <a href="login.php">Login here</a>.</p>
+        </form>
+    </div>    
+</body>
+</html>
